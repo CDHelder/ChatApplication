b8e063a77c05859b6d746ee968e2c6652f22cb53
C Helder <christian.helder@itvitaelearning.nl>
2021-11-05 15:49:00 +0100

    GenericRepository & UnitOfWork Done

--- ChatApplication.Data.Service/Interfaces/IUnitOfWork.cs
+++ ChatApplication.Data.Service/Interfaces/IUnitOfWork.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatApplication.Data.Service.SpecificRepositories;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,11 @@ namespace ChatApplication.Data.Service.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
+        UoFApplicationUserRepository ApplicationUserRepository { get; }
+        UoFGroupChatRepository GroupChatRepository { get; }
+        UoFMessageRepository MessageRepository { get; }
+        UoFPrivateChatRepository PrivateChatRepository { get; }
+        UoFPublicChatRepository PublicChatRepository { get; }
         bool SaveChanges();
     }
 }
---
+++ ChatApplication.Data.Service/UnitOfWork.cs
@@ -0,0 +1,65 @@
+﻿using ChatApplication.Data.Service.Interfaces;
+using ChatApplication.Data.Service.SpecificRepositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatApplication.Data.Service
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private UoFApplicationUserRepository _applicationUserRepository;
+        private UoFGroupChatRepository _groupChatRepository;
+        private UoFMessageRepository _messageRepository;
+        private UoFPrivateChatRepository _privateChatRepository;
+        private UoFPublicChatRepository _publicChatRepository;
+
+        public UnitOfWork(ApplicationDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
+
+        public UoFApplicationUserRepository ApplicationUserRepository 
+        {
+            get => _applicationUserRepository ?? new UoFApplicationUserRepository(_dbContext);
+            private set => _applicationUserRepository = value;
+        }
+        public UoFGroupChatRepository GroupChatRepository 
+        {
+            get => _groupChatRepository ?? new UoFGroupChatRepository(_dbContext);
+            private set => _groupChatRepository = value;
+        }
+        public UoFMessageRepository MessageRepository 
+        {
+            get => _messageRepository ?? new UoFMessageRepository(_dbContext);
+            private set => _messageRepository = value;
+        }
+        public UoFPrivateChatRepository PrivateChatRepository 
+        {
+            get => _privateChatRepository ?? new UoFPrivateChatRepository(_dbContext);
+            private set => _privateChatRepository = value;
+        }
+        public UoFPublicChatRepository PublicChatRepository 
+        {
+            get => _publicChatRepository ?? new UoFPublicChatRepository(_dbContext);
+            private set => _publicChatRepository = value;
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        public bool SaveChanges()
+        {
+            var rowsChanged = _dbContext.SaveChanges();
+            if (rowsChanged > 0)
+                return true;
+
+            return false;
+        }
+    }
+}
